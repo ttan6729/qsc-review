1452cfe3d31ac24a239a156bbde5d3d3430f1588
ubuntu <13183963@student.uts.edu.au>
2021-06-07 08:37:25 +0000

    update

---
+++ Crumble/run.sh
@@ -0,0 +1,5 @@
+nohup bash -c '/usr/bin/time -v crumble -O bam SRR032209.bam SRR032209.bam.crumble.cmp' </dev/null 1> SRR032209.out 2> SRR032209.err
+nohup bash -c '/usr/bin/time -v crumble -O bam SRR13575706_1.bam SRR13575706_1.bam.crumble.cmp' </dev/null 1> SRR13575706_1.out 2> SRR13575706_1.err
+nohup bash -c '/usr/bin/time -v crumble -O bam SRR5220205_1.bam SRR5220205_1.bam.crumble.cmp' </dev/null 1> SRR5220205_1.out 2> SRR5220205_1.err
+nohup bash -c '/usr/bin/time -v crumble -O bam SRR7216025_1.bam SRR7216025_1.bam.crumble.cmp' </dev/null 1> SRR7216025_1.out 2> SRR7216025_1.err
+
